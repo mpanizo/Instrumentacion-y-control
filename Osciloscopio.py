e708ddd774dd79d9e4fc654c4775aa9d51592ddc
Micaela <miica.vl@hotmail.com>
2019-04-22 18:29:52 -0300

    Agregamos funcionalidades y casi cerramos para programar ambos

--- Osciloscopio.py
+++ Osciloscopio.py
@@ -1,38 +1,36 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar 26 18:39:33 2019
-
 @author: nicon
 """
 
-import pyvisa
 import numpy as np
 import matplotlib.pyplot as plt
+import lantz 
+from lantz import MessageBasedDriver, Feat #feat: decorador
+from lantz.core import mfeats
 
-class Osciloscopio(object):
-
-    def __init__(self, instrument = 0): #Ver cual es 1 y 0 con el generador
-        self.rm = pyvisa.ResourceManager()
-        if len(self.rm.list_resources()) > 0:
-            self.inst = self.rm.open_resource(self.rm.list_resources()[instrument])
-        else:
-            self.inst = []
-            print('No se detectó ningún instrumento')
-        if self.inst != []:
-            try:
-                print('El IDN del instrumento es ', self.inst.query("*IDN?"))
-            except:
-                print('El instrumento no respondió cuando se le preguntó el nombre.')
+class Osciloscopio(MessageBasedDriver):
         
+    @Feat(units = 's') #eso debe usar pint para setear las unidades
+    def timebase(self):
+        return self.query('HOR:MAIN:SCA?')
+    
+    @timebase.setter
+    def timebase(self,base):
+        self.write('HOR:MAIN:SCA {}'.format(base))
+    
+    timebase2 = mfeats.QuantityFeat('HOR:MAIN:SCA?', 'HOR:MAIN:SCA {}', units='s', limits=(0.001, 100)) #esto es todo lo anterior en una sola linea
+    
     def data_encdg_ascii(self):
-        self.inst.write('DATA:ENCDG ASCII')
+        self.write('DATA:ENCDG ASCII')
         
     def data_encdg_bin(self):
-        self.inst.write('DATA:ENCDG RIBinary')
+        self.write('DATA:ENCDG RIBinary')
 
     def get_data_ascii(self):
         self.data_encdg_ascii()
-        read = self.inst.query_ascii_values('CURVe?')
+        read = self.resource.query_ascii_values('CURVe?')
         
         plt.plot(read)
         plt.show()
@@ -47,11 +45,11 @@ class Osciloscopio(object):
     def read_voltage(self):
         
         self.data_encdg_bin()
-        read = np.array(self.inst.query_binary_values('CURVe?', datatype = 'b', is_big_endian= True))
+        read = np.array(self.resource.query_binary_values('CURVe?', datatype = 'b', is_big_endian= True))
         
-        ymult = self.inst.query_ascii_values('WFMPRE:YMULT?') #Vertical scale factor
-        yzero = self.inst.query_ascii_values('WFMPRE:YZERO?') #Offset Voltage
-        yoff = self.inst.query_ascii_values('WFMPRE:YOFF?')   #Vertical Offset
+        ymult = self.resource.query_ascii_values('WFMPRE:YMULT?') #Vertical scale factor
+        yzero = self.resource.query_ascii_values('WFMPRE:YZERO?') #Offset Voltage
+        yoff = self.resource.query_ascii_values('WFMPRE:YOFF?')   #Vertical Offset
 
         voltage = yzero + ymult*(read - yoff)
         
@@ -62,9 +60,9 @@ class Osciloscopio(object):
     
        
     def read_time(self):    
-        xincr = self.inst.query_ascii_values('WFMPRE:XINCR?') #Horizontal sampling interval
-        xzero = self.inst.query_ascii_values('WFMPRE:XZERO?')
-        pt_off = self.inst.query_ascii_values('WFMPRE:PT_Off?')
+        xincr = self.resource.query_ascii_values('WFMPRE:XINCR?') #Horizontal sampling interval
+        xzero = self.resource.query_ascii_values('WFMPRE:XZERO?')
+        pt_off = self.resource.query_ascii_values('WFMPRE:PT_Off?')
         
         n = np.linspace(0,2500,2500)
         #Ver https://www.i3detroit.org/wi/images/2/2d/460-ProgrammerManual.pdf pag 2-43
@@ -87,4 +85,15 @@ class Osciloscopio(object):
 
     
     def set_timebase(self, seconds):
-        self.inst.write('HOR:DEL:SCA {}'.format(seconds))
+        self.write('HOR:DEL:SCA {}'.format(seconds))
+        
+ #%%
+if __name__ == '__main__':
+
+    with Osciloscopio.via_usb('C065092') as Osc:
+        Osc.grafico()
+
+    with Osciloscopio.via_usb('C065092') as Osc:
+        Osc.timebase = 0.01 #setter; lo setea
+        print(Osc.timebase) #este te lo devuelve. esto esta bueno porque te queda timebase como propiedad en vez de tener un setter y un getter
+#podria ponerlo con pint directamente como osci.timebase = 0.01 * ureg.seconds
